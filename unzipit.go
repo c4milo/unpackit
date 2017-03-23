529412b88e354d3142f9d0a20de15894c7751ff7
Camilo Aguilar <camilo.aguilar@gmail.com>
2017-03-23 07:28:35 -0500

    Update package docs

--- unzipit.go
+++ unzipit.go
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-// Package unzipit allows you to easily unpack *.tar.gz, *.tar.bzip2, *.zip and *.tar files.
+// Package unzipit allows you to easily unpack *.tar.gz, *.tar.bzip2, *.tar.xz, *.zip and *.tar files.
 // There are not CGO involved nor hard dependencies of any type.
 package unzipit
 
